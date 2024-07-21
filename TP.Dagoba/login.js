3f2c80dadb0930df6f7404a30f697db49dc1671c
Prophase90 <danieligale@gmail.com>
2024-07-21 13:17:41 -0300

    Responsive_v1

--- TP.Dagoba/login.js
+++ TP.Dagoba/login.js
@@ -1,8 +1,6 @@
-// Get the modal
 var modal1 = document.getElementById('id01');
 var modal2 = document.getElementById('id02');
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal1) {
         modal1.style.display = "none";
@@ -10,4 +8,10 @@ window.onclick = function(event) {
     if (event.target == modal2) {
         modal2.style.display = "none";
     }
+}
+
+function toggleMenu() {
+    event.preventDefault();
+    var menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
 }
