3633aa5517934edb871372eadf2383ab08a97c6d
Prophase90 <danieligale@gmail.com>
2024-07-30 20:24:26 -0300

    Final_Last_Version_v1

--- TP.Dagoba/script.js
+++ TP.Dagoba/script.js
@@ -616,3 +616,24 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " activar";
 }
+
+// Get the button
+let mybutton = document.getElementById("myBtnUp");
+console.log('Botón:', mybutton);
+
+/* When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}*/
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
